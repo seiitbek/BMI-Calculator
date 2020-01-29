3ab3058d8fb2fad81e9d30b0fea013b6731960ea
seiitbek <seiitbek99@gmail.com>
2020-01-29 18:28:48 +0600

    first commit

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Zhandos on 1/29/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
